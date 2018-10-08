58e11a5ab40f38c6eea460b89a295bec0e972c54
Sidra Sarwar <43807412+sidra888@users.noreply.github.com>
2018-10-08 09:49:15 +0500

    Update 17-ag-3684 Yield.py

--- Assignment 2/17-ag-3684 Yield.py
+++ Assignment 2/17-ag-3684 Yield.py
@@ -8,6 +8,7 @@ import statsmodels.api as sm
 from mpl_toolkits.mplot3d import Axes3D
 from statsmodels.formula.api import ols
 import pandas as pd
+
 #1 way to combine data
 #Upload excel file
 #fg=pd.read_csv("add.csv")
@@ -30,28 +31,40 @@ rt = Reg2.fit()
 print("Values of regression coefficients and intercept")
 print(rt.params)
 gt=np.array(rt.fittedvalues)
+
+#MEAN VALUE OF YIELD
 num_items = len(Yield)
 mean = sum(Yield) / num_items
+
+#TOTAL SS
 differences = [Yield - mean for Yield in Yield]
 sq_differences = [d ** 2 for d in differences]
 op = np.sum(sq_differences)
 print('The  TOTAL sum of square(TSS) is =',op)
+#REGRESSION SS
 d = np.array([gt - 60])
 jk = np.array([d ** 2 for d in d])
 ikl = np.sum(jk)
 print('The REGRESSION SUM OF SQUARE (RSS)is =',ikl)
+
+#ERROR SS
 differences = [Yield - gt]
 sq_differences = [d ** 2 for d in differences]
 ssd = np.sum(sq_differences)
 print('The ERROR sum of square is(ESS) =.',ssd)
+
+#COEFFICIENT OF DETERMINATION
 print("R SQUARE COEFFICIENT OF DETERMINATION")
 print(ikl/op)
+
 #Summary of Whole Data
 print(rt.summary())
+
 print ("ANOVA TABLE FOR GIVEN DATA")
 mod = ols('Yield ~ Fert + Rainfall', data=fg).fit()
 aov_table = sm.stats.anova_lm(mod, typ=2)
 print(aov_table)
+
 #ONe Sample T Test
 print("One Sample T Test ")
 one=stats.ttest_1samp(Fert, 0.0)
@@ -60,12 +73,14 @@ one=stats.ttest_1samp(Rainfall, 0.0)
 print(one)
 one=stats.ttest_1samp(Yield, 0.0)
 print(one)
+
 #Scatter Plot
 ax.scatter(Fert,Rainfall,Yield,c='blue',marker='o')
 ax.set_xlabel("Fertilizer")
 ax.set_ylabel("Rainfall")
 ax.set_zlabel("Yield")
 plt.show()
+
 #PLOT Histogram
 fg.hist(bins=50, figsize=(20,15))
 plt.savefig("attribute_histogram_plots")
